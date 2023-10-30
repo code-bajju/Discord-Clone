9bb7a6a322bed984bb2327313cc556c04e8eec7d
code-bajju <11212766@mmec.mmdu.org>
2023-10-30 19:01:52 +0530

    Boiler removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-import Image from 'next/image'
+// import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+
+const state = true
 
 export default function Home() {
   return (
-  <p className='text-3xl font-bold text-indigo-200'>hello discord clone</p>
+    <div >
+  <p className="text-3xl font-bold text-indigo-200">hello discord clone</p>
+  <Button className={cn("bg-indigo-500" , state && "bg-red-500" )} >click me</Button></div>
   )
 }
