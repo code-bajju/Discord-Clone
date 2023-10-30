79974ebda5f0a6614d4b162178151258a15cff40
code-bajju <11212766@mmec.mmdu.org>
2023-10-30 19:03:33 +0530

    cn changed state

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
 
-const state = true
+// const state = true
 
 export default function Home() {
   return (
     <div >
   <p className="text-3xl font-bold text-indigo-200">hello discord clone</p>
-  <Button className={cn("bg-indigo-500" , state && "bg-red-500" )} >click me</Button></div>
+  <Button >click me</Button></div>
   )
 }
