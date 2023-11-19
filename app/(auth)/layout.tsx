1c125199717ee33311d835450100afe184d8bd5c
code-bajju <11212766@mmec.mmdu.org>
2023-11-19 21:35:58 +0530

    added env files and clerk for authentication

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const AuthLayout = ({children}:{children : React.ReactNode}) => {
-    return (<div className="bg-red-500 h-full">{children}</div> );
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+    <div className="h-full m-20 flex item-center justify-center">
+        {children}
+    </div>
+    );
 }
- 
+
 export default AuthLayout;
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-// import Image from 'next/image'
-import { Button } from '@/components/ui/button'
-import { cn } from '@/lib/utils'
-
-// const state = true
-
+import { UserButton } from "@clerk/nextjs";
+ 
 export default function Home() {
   return (
-    <div >
-  <p className="text-3xl font-bold text-indigo-200">hello discord clone</p>
-  <Button >click me</Button></div>
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   )
 }
