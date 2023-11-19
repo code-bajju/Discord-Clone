1e68899d3ecf1170e645083fd2972eb3d79ec9e1
code-bajju <11212766@mmec.mmdu.org>
2023-11-19 22:22:30 +0530

    Theme added with toggle button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,36 @@
-import type { Metadata } from 'next'
-import { Open_Sans } from 'next/font/google'
-import './globals.css'
-import { ThemeProvider } from '@/components/providers/theme-provider'
-import { ClerkProvider } from '@clerk/nextjs'
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ClerkProvider } from "@clerk/nextjs";
+import { cn } from "@/lib/utils";
 
-const font = Open_Sans({ subsets: ['latin'] })
+const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Discord Clone',
-  description: 'Next js Learning Project',
-}
+  title: "Discord Clone",
+  description: "Next js Learning Project",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
-    <html lang="en" suppressHydrationWarning>
-      <body className={font.className}><ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false} storageKey='discord-theme'>{children}</ThemeProvider></body>
-    </html>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn(font.className,"bg-white dark:bg-[#313338]")}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="discord-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
     </ClerkProvider>
-  )
+  );
 }
