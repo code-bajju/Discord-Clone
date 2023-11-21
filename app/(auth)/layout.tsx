567b686273b7d92695555afa17651cfa1fb6765d
code-bajju <11212766@mmec.mmdu.org>
2023-11-21 14:45:37 +0530

    Schema Updated for user

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
     return (
     <div className="h-full m-20 flex item-center justify-center">
